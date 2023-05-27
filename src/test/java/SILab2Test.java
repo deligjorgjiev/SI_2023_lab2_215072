8d08d29e0a6a5534009a0950dd7b5f429250296f
Hristijan Deligjorgjiev <hristijan.deligjorgjiev@students.finki.ukim.mk>
2023-05-27 17:26:45 +0200

    Create SILab2Test.java

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    @Test
+    void testEveryBranch() {
+        testA();
+        testB();
+        testC();
+        testD();
+        testE();
+    }
+
+    @Test
+    void testMC() {
+        testA();
+        assertThrows(RuntimeException.class, () -> SILab2.function(
+                new User("username123", null, "hristijan@mail.com"), null));
+        assertThrows(RuntimeException.class, () -> SILab2.function(
+                new User("username123", "p@ssword!", null), null));
+
+        List<User> allUsers = new ArrayList<>(1);
+        allUsers.add(new User("username123", "password.", "hristijan@mail.com"));
+        assertDoesNotThrow(() -> SILab2.function(
+                new User("username123", "p@ssword!", "hristijan@mail.com"), allUsers)); //nullPointerException if used null instead of allUsers list
+    }
+
+    void testA(){
+        assertThrows(RuntimeException.class, () -> SILab2.function(null, null));
+    }
+
+    void testB(){
+        User user = new User(null, "p@ssword!", "hristijan@mail.com");
+        List<User> allUsers = new ArrayList<>(1);
+        allUsers.add(new User("username123", "password.", "hristijan@mail.com"));
+        assertFalse(SILab2.function(user, allUsers));
+    }
+
+    void testC(){
+        User user = new User(null, "passwordd", "hristijan@mail.com");
+        List<User> allUsers = new ArrayList<>(1);
+        allUsers.add(new User("username123", "password.", "hristijan@mail.com"));
+        assertFalse(SILab2.function(user, allUsers));
+    }
+
+    void testD(){
+        User user = new User(null, "pass word", "hristijan@mail.com");
+        List<User> allUsers = new ArrayList<>(1);
+        allUsers.add(new User("hristijan@mail.com", "password.", "email@email.co.uk"));
+        assertFalse(SILab2.function(user, allUsers));
+    }
+
+    void testE(){
+        User user = new User("deligjorgjiev", "deli", "hristijan at mail dot com");
+        assertFalse(SILab2.function(user, null)); //null list is okay, because of the email, the list elements are not checked through
+    }
+}
